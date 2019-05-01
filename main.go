bc202dc28a5c57702739235ebd82cf6dbcbb3d3d
rastasheep <rastasheep3@gmail.com>
2019-05-01 23:04:36 +0200

    Listen to clicks on canvas

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ type Canvas struct {
 	height  float64
 	width   float64
 	element js.Value
+	onClick js.Func
 }
 
 func NewCanvas(width float64, height float64, element js.Value) *Canvas {
@@ -35,6 +36,29 @@ func (canvas *Canvas) Render(img *image.RGBA) {
 	ctx.Call("putImageData", canvasData, 0, 0)
 }
 
+func (canvas *Canvas) AttachOnClick() {
+	callback := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		rect := args[0].Get("target").Call("getBoundingClientRect")
+		x := args[0].Get("clientX").Int() - rect.Get("left").Int()
+		y := args[0].Get("clientY").Int() - rect.Get("top").Int()
+
+		x, y = canvas.ParseCoordinates(x, y)
+
+		fmt.Println("click", x, y)
+		return nil
+	})
+	canvas.onClick = callback
+	canvas.element.Call("addEventListener", "click", callback)
+}
+
+func (canvas *Canvas) RemoveOnClick() {
+	canvas.onClick.Release()
+}
+
+func (canvas *Canvas) ParseCoordinates(x int, y int) (int, int) {
+	return x, y
+}
+
 func loadImage() *image.RGBA {
 	img := image.NewRGBA(image.Rect(0, 0, 700, 900))
 	white := color.RGBA{255, 255, 255, 255}
@@ -43,20 +67,20 @@ func loadImage() *image.RGBA {
 }
 
 func main() {
-
 	el := js.Global().Get("document").Call("getElementById", "canvas")
 	canvas := NewCanvas(700, 900, el)
 
 	img := loadImage()
-
 	canvas.Render(img)
+
+	canvas.AttachOnClick()
+	defer canvas.RemoveOnClick()
 
 	resizeCallback := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		fmt.Println("resize")
 		return nil
 	})
-	defer renderFrame.Release()
-
+	defer resizeCallback.Release()
 	js.Global().Call("addEventListener", "resize", resizeCallback)
 
 	done := make(chan struct{}, 0)
